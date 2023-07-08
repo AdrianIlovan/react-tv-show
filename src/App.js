5aee1588f9224a6f01b350be6fb882930ba683b3
Adrian Ilovan <adrian88ilovan@yahoo.com>
2023-07-08 15:23:08 +0100

    1st update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import { Shows } from './Shows';
 
 function App() {
+  const [shows, setShows] = useState([]);
+  
+  useEffect(() => {
+    fetch("https://api.tvmaze.com/shows")
+    .then((res) => res.json())
+    .then((json) => setShows(json));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {shows.map((show) => (
+        <div>
+          <Shows movieName = {show.name} />
+      ))}
     </div>
   );
 }
---
+++ src/Shows.js
@@ -0,0 +1,7 @@
+export const Shows = (props) => {
+    return (
+        <div>
+            {}
+        </div>
+    )
+}
